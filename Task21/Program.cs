016d93076866e65c52f3d93fa96b5070d5f1633c
Anastasia <miki_nastya@mail.ru>
2023-09-26 20:09:55 +0800

    Добавили Task21 и Task23

---
+++ Task21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программа, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3d пространстве.
+
+int GetUserInput(string message)
+{
+    Console.Write($"{message}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+// d = √((хА –  хВ)2 + (уА – уВ)2)
+double Distance(int x1, int y1, int x2, int y2, int z1, int z2)
+{
+    double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+    return res;
+}
+
+int x1 = GetUserInput("Координаты первой точки\nВведите координату X");
+int y1 = GetUserInput("Координаты первой точки\nВведите координату Y");
+int z1 = GetUserInput("Координаты первой точки\nВведите координату Z");
+Console.WriteLine("------------------------------");
+int x2 = GetUserInput("Координаты второй точки\nВведите координату X");
+int y2 = GetUserInput("Координаты второй точки\nВведите координату Y");
+int z2 = GetUserInput("Координаты первой точки\nВведите координату Z");
+
+double distance = Distance(x1, y1, x2, y2, z1, z2);
+Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
---
+++ Task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдает таблицу кубов чисел от 1 до N.
+
+void SquareNumbers (int num) 
+{
+    int count = 1;
+    while (count <= num) {
+        Console.WriteLine($"{count, 3} -> {count*count*count, 5}");
+        count++;
+    }
+}
+
+
+Console.WriteLine ("Введите число:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+SquareNumbers(userNum);
